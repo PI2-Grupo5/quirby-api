bff17a72b92fdcd2512589ad82844a1aa8d068c7
marcos-mv <vinicius_kof36@hotmail.com>
2023-02-09 15:02:28 -0300

    Adicionando verificador de identidade de dominio

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,16 @@
 var express = require('express');
 
 var routes = express.Router();
+const path = require('path')
 
 const robotRouter = require('./persons.js');
 const personRouter = require('./robot.js');
 
 routes.use('/', personRouter);
 routes.use('/', robotRouter);
+
+routes.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, '../view/index.html'));
+});
 
 module.exports = routes;
